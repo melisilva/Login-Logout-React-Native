81c4d1a21f272a06cb2d3bc451c0af35ab2ecdfd
melisilva <melissa.silva2001.ms@gmail.com>
2023-02-07 16:14:31 +0000

    Expo app started with home screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  
+
+  return (
+    <View style={styles.container}>
+
+        <Text style={styles.buttonText}>Sign out</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+   button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
